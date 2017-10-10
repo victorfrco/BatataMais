4eb17ad6fccd294e12ae66344a87577b0420493a
victor.oliveira <chico1996>
2017-10-10 18:11:08 -0400

    Alteração completa para laravel 5.4

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,14 +11,18 @@
 |
 */
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker\Generator $faker) {
+    static $password;
+
     return [
         'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
 });
+
 $factory->define(App\Models\Product::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->sentence(2),
