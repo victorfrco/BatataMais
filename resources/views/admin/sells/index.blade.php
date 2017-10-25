9c5f82b4715c80c63ccc0b3cd521b1f85916963d
victor.oliveira <chico1996>
2017-10-25 17:58:50 -0300

    CRIAÇÃO DE ARQUITETURA PARTE 1

---
+++ resources/views/admin/sells/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h2>PÁGINA DE VENDAS</h2>
+        </div>
+    </div>
+@endsection
