6c01abdacc66a39e7417e574d09c1857c5b74705
victor.oliveira <chico1996>
2017-10-31 17:07:22 -0300

    inserção de div de marca nas categorias

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,18 @@
     <div class="container">
         <div class="row">
             <h2>PÁGINA TESTE</h2>
-        </div>
+            <div class="col-sm-2" style="height: 150px; max-width: 150px; font-size: 12px; font-weight:bold; text-align: center;
+            border: 2px solid grey; border-radius: 8px;">
+                {!! 'chico'.Image::rounded('https://seeklogo.com/images/C/Coca-Cola-logo-00A6B20F2F-seeklogo.com.png', 'rounded')
+                    ->responsive()
+                    ->withAttributes(['style' => 'max-height:120px; ']);
+                !!}
+            </div>
+    </div>
         <div class="row">
             <div class="col-sm-8" style="background-color:lightblue; height: 400px;" id="tabsCategorias" data-url="<?= route('admin.categories.create') ?>">
                 @php
+
                     foreach($categories as $category){
                         $brands = App\Models\Brand::all()->where('category_id', '=', $category->id);
                         $table = Table::withContents($brands)->hover();
