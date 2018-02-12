10aeb096374c494a42a53629288c800f2ff637d9
Victor Oliveira <victorfrco3@hotmail.com>
2018-02-12 14:52:05 -0300

    nome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ 'Conceito Tabacaria' }}</title>
+    <title>{{ 'Batata+' }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
