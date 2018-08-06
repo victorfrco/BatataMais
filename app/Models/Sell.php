234fa59f744143695b9d5c0eb32c84db789bed9c
Victor Oliveira <victorfrco3@hotmail.com>
2018-08-05 20:34:15 -0400

    alinhado grid de itens de venda

--- app/Models/Sell.php
+++ app/Models/Sell.php
@@ -12,18 +12,15 @@ class Sell extends Model
     public static function atualizaTabelaDeItens($order_id)
     {
         $order = Order::find($order_id);
-//        $itens = Item::all()->where('order_id', '=', $order_id);
-//        $itens = Item::all()->where('order_id', '=', $order_id);
         $itens = DB::table('itens')->select('*')->where('order_id','=',$order_id)->orderByDesc('updated_at')->get()->toArray();
         $itens = Item::hydrate($itens);
-//        dd($itens, $itens2, $itens3);
         $tableHeader = '<table class="table" style="font-size: 13px">
                             <tr>
                                 <th>Descrição</th>
-                                <th>Qtd.</th>
-                                <th>Valor Unid.</th>
-                                <th>Valor Total</th>
-                                <th style="text-align: center">'.Icon::remove().'</th>
+                                <th style="text-align: center;">Qtd.</th>
+                                <th style="text-align: center;">Valor Unid.</th>
+                                <th style="text-align: center;">Valor Total</th>
+                                <th style="text-align: center; color: #2a88bd; font-size: 15px">'.Icon::remove().'</th>
                             </tr>';
         $tableCont = [];
         foreach ($itens as $item){
@@ -32,16 +29,16 @@ class Sell extends Model
                 $price = $product->price_discount;
             else if($order->pay_method == 3)
                 $price = $product->price_card;
-	        else
-	        	$price = $product->price_resale;
+            else
+                $price = $product->price_resale;
 
 
             $tupla = '      <tr>
-                                <td align="left">'.$product->name.'</td>
-                                <td align="right">'.$item->qtd.'</td>
-                                <td align="right">'.number_format($price, 2, ',', '.').'</td>
-                                <td align="right">'.number_format($item->total, 2, ',', '.').'</td>
-                                <td align="center">'.Button::link(Icon::remove())->asLinkTo(route('removeItem', ['item' => $item])).'</td>
+                                <td style="vertical-align: middle" align="left">'.$product->name.'</td>
+                                <td align="center" style="vertical-align: middle">'.$item->qtd.'</td>
+                                <td align="center" style="vertical-align: middle; text-shadow: 1px 1px #ffcc00; color: #ffffff;">'.number_format($price, 2, ',', '.').'</td>
+                                <td align="center" style="vertical-align: middle; text-shadow: 1px 1px #ffcc00; color: #ffffff;">'.number_format($item->total, 2, ',', '.').'</td>
+                                <td align="center" style="vertical-align: middle">'.Button::link(Icon::remove())->asLinkTo(route('removeItem', ['item' => $item])).'</td>
                             </tr>';
             array_push($tableCont, $tupla);
         }
