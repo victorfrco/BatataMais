b22f942edd9056c2de53acabc2c9a6c836c41de2
Victor Oliveira <victorfrco3@hotmail.com>
2018-08-09 11:44:25 -0400

    alteração do nome do site para "Minha Loja"

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Federal Hookah Pub'),
+    'name' => env('APP_NAME', 'Minha Loja'),
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ 'Batata+' }}</title>
+    <title>{{ 'Minha Loja' }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
